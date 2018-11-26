e7024c149699abdf91d1edc41ad4f5351c1ac917
Ekoban <robinet.alexandre@gmail.com>
2018-11-26 07:21:48 +0100

    New refactoring

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const geocodeAddress = (address) => {
+const geocodeAddress = (address, callback) => {
     let encodedAddress = encodeURIComponent(address);
     let URL = `http://www.mapquestapi.com/geocoding/v1/address?key=VCgwinoGdXf0kuSNf9FjK5Ydor5zQE0E&location=${encodedAddress}`;
     
@@ -13,29 +13,24 @@ const geocodeAddress = (address) => {
     (error, response, body) => { //callback
         let statusCode = response.body.info.statuscode;
         let statusText = response.body.info.messages[0];
+        let latitude = body.results[0].locations[0].latLng.lat;
+        let longitude = body.results[0].locations[0].latLng.lng;
+        let street = body.results[0].locations[0].street;
+        let city = body.results[0].locations[0].adminArea5;
+        let country = body.results[0].locations[0].adminArea1 ;
+        
+        
         console.log('-----------------------------------');
         console.log(`Status Code: ${statusCode}, Status Text: ${statusText}`);
         console.log('-----------------------------------');
         
-        if(error) {
-            console.log(`
-            ====================================
-            Unable to connect to MapQuest servers.
-            ====================================
-            `);
-        } else if (statusCode === 400) {
-            console.log(`
-            ====================================
-            Unable to find this address.
-            ====================================
-            `);
-        } else if (statusCode === 0) {
-            let latitude = body.results[0].locations[0].latLng.lat;
-            let longitude = body.results[0].locations[0].latLng.lng;
-            let street = body.results[0].locations[0].street;
-            let city = body.results[0].locations[0].adminArea5;
-            let country = body.results[0].locations[0].adminArea1 ;
+        /*---------------------------------------------------------*/
 
+        if(error) {
+            callback('Unable to connect to MapQuest servers.')
+        } else if (statusCode === 400) {
+            callback('Unable to find this address.')
+        } else if (statusCode === 0) {
             console.log('====================================');
             console.log(`Address found: ${street}, ${city}, ${country}.`);
             console.log('-----------------------------------');
